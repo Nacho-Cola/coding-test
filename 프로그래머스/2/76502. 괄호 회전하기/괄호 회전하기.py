b09c9921e306a23a7d8020c9964864a9aab1970d
Nacho-Cola <61578392+Nacho-Cola@users.noreply.github.com>
2023-12-11 17:27:49 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,23 +1,19 @@
 def solution(s):
-    answer = []
-    n = 0;
+    corr = 0;
     for i in range(0,len(s)):
-        answer =[]
+        stack =[]
         j = 0
         for bra in s:
             if bra == '[' or bra == '(' or bra =='{':
-                answer.append(bra)
-            if len(answer):
-                if answer[-1] == '(' and bra == ')':
-                    answer.pop()
-                elif answer[-1] == '{' and bra == '}':
-                    answer.pop()
-                elif answer[-1] == '[' and bra == ']':
-                    answer.pop()
+                stack.append(bra)
+            if len(stack):
+                if stack[-1] == '(' and bra == ')':stack.pop()
+                elif stack[-1] == '{' and bra == '}':stack.pop()
+                elif stack[-1] == '[' and bra == ']':stack.pop()
             else:
                 j+=1
-        if len(answer) == 0 and j!=len(s):
-            n+=1
+        if len(stack) == 0 and j!=len(s):
+            corr+=1
         s = s + s[0]
         s = s[1:]
-    return n
+    return corr
