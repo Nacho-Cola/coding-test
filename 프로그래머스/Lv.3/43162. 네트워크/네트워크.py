d77a20a0441e0ea89af936d3149252e69942f176
Nacho-Cola <61578392+Nacho-Cola@users.noreply.github.com>
2024-01-14 12:34:24 +0900

    [level Lv.3] Title: 네트워크, Time: 0.23 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/Lv.3/43162. 네트워크/네트워크.py
+++ 프로그래머스/Lv.3/43162. 네트워크/네트워크.py
@@ -1,16 +1,15 @@
-def bfs(computers, v, visited):
+def bfs(computer, v, visited):
     visited[v] = True
-    for w in range(len(computers[0])):
-        if computers[v][w] == 1 and not visited[w]:
-            bfs(computers, w, visited)
-        
-        
+    for w in range(len(computer[0])):
+        if computer[v][w] == 1 and not visited[w]:
+            bfs(computer, w, visited)
+    
 
 def solution(n, computers):
     answer = 0
     visited = [False] * n
     for i in range(n):
         if not visited[i]:
+            answer+=1
             bfs(computers, i, visited)
-            answer += 1
     return answer
