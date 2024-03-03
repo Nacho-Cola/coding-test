d9830fc1d9c7f8d50c51da2e14188c897e0cbf0a
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-03-04 00:03:25 +0900

    [level 3] Title: 등굣길, Time: 1.58 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,12 +1,15 @@
 def solution(m, n, puddles):
-    a = [[0]*(m+1) for _ in range(n+1)]
-    for i, j in puddles:
-        a[j][i] = 1
-    a[0][1] = 1
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            if a[i][j]:
-                a[i][j] = 0
+    table = [[0]*(m+1) for _ in range(n+1)]
+    
+    for i,j in puddles:
+        table[j][i] = 1
+    
+    table[0][1] = 1
+    
+    for i in range(1,n+1):
+         for j in range(1,m+1):
+            if table[i][j]:
+                table[i][j] = 0
             else:
-                a[i][j] = (a[i-1][j]+a[i][j-1]) % 1_000_000_007
-    return a[-1][-1]
+                table[i][j] = (table[i-1][j] + table[i][j-1]) % 1000000007
+    return table[-1][-1]
