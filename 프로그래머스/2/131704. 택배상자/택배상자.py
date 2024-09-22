2b5085e8ace22814b79a4cef7cce06c26f64f387
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-09-22 14:53:01 +0900

    [level 2] Title: 택배상자, Time: 514.31 ms, Memory: 53.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.py
@@ -0,0 +1,37 @@
+def solution(order):
+    answer = 0
+    stack = []
+    j = 0
+    i = 0
+    
+    while i < len(order):
+        
+        if i+1 == order[j]:
+            answer+=1
+            j+=1
+            i+=1
+            
+        elif stack and stack[-1]==order[j]:
+            
+            for k in stack[::-1]:
+                if k == order[j]:
+                    answer+=1
+                    j+=1
+                    stack.pop()
+                else:
+                    break
+        else:
+            stack.append(i+1)
+            i+=1
+        
+    if stack and stack[-1]==order[j]:
+
+        for k in stack[::-1]:
+            if k == order[j]:
+                answer+=1
+                j+=1
+                stack.pop()
+            else:
+                break
+        
+    return answer
