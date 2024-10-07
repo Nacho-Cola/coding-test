fe5cd20bfc04a6594092f3ef25728ebe01234173
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-10-07 16:02:27 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 204.11 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+def solution(sequence):
+    seq = [num * (-1)**(i%2) for i,num in enumerate(sequence)]
+    
+    for i in range(len(sequence)-1):
+        seq[i+1] += seq[i]
+        
+    return max(abs(min(seq)),max(seq),max(seq)-min(seq))
