958b6b4f5279407fd607c8dac73d35011f1def11
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-10-14 17:25:35 +0900

    [level 2] Title: 배달, Time: 3.24 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.py
+++ 프로그래머스/2/12978. 배달/배달.py
@@ -6,20 +6,21 @@ def dijkstra(distance, graph):
     while heap:
         node, cost = heappop(heap)
         for n, c in graph[node]:
-            if cost + c < distance[n]:
-                distance[n] = cost + c
-                heappush(heap, [n, cost+c])
-    
+            if cost+c < distance[n]:
+                distance[n] = cost+c
+                heappush(heap,[n,cost+c])
 
 def solution(N, road, K):
-    distance = [500001]*(N+1)
+    answer = 0
+    
+    distance = [500001] * (N+1)
     distance[1] = 0
-    graph = [[] * (N+1) for _ in range(N+1)]
+    graph = [[] for _ in range(N+1)]
     
     for a, b, cost in road:
         graph[a].append((b,cost))
         graph[b].append((a,cost))
     
-    dijkstra(distance,graph)
-    
-    return len([i for i in distance if i <= K])
+    dijkstra(distance, graph)
+
+    return len([i for i in distance if i<=K])
