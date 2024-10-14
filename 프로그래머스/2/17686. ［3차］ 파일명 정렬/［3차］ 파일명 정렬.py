959fd343e427046a388c1a54c9260d95f17e4633
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-10-14 15:17:21 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.84 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -2,7 +2,7 @@ import re
 
 
 def solution(file_names):
-    filt = re.compile(r'([a-zA-Z\-\n.\s]+)([0-9]{0,5})(.*)')
+    filt = re.compile(r'([a-zA-Z\-\.\s]+)([0-9]{0,5})(.*)')
     files = []
     for file_name in file_names:
         files.extend(filt.findall(file_name))
