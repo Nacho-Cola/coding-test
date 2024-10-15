e4e8d927451e6d7690db02332e89f53dc3043e34
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-10-16 01:11:34 +0900

    [level 4] Title: 단어 퍼즐, Time: 38.27 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/4/1882. 단어 퍼즐/단어 퍼즐.py
@@ -0,0 +1,18 @@
+def solution(strs, t):
+    strs = set(strs)
+    n = len(t)
+    dp = [0] * (n+1)
+    
+    for i in range(1,n+1):
+        dp[i] = float('inf')
+        for j in range(1,6):
+            if i - j < 0 :
+                continue
+            else:
+                s = i - j
+                if t[s:i] in strs:
+                    dp[i] = min(dp[i], dp[i-j]+1)
+
+    if dp[-1] == float('inf'):
+        return -1
+    return dp[-1]
