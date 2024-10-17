fda41e76dd775effc1eed81e2ce223d261f0b8aa
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-10-18 02:45:18 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.85 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -1,24 +1,19 @@
+import re
 from collections import Counter
-
-def get_pair(string):
-    pair = []
-    for i in range(len(string)-1): 
-        if string[i].isalpha():
-            if string[i+1].isalpha():
-                pair.append(string[i]+string[i+1])
-    return pair
-
 def solution(str1, str2):
-    answer = 1
-    str1 = str1.upper()
-    str2 = str2.upper()
-    pair1 = get_pair(str1)
-    pair2 = get_pair(str2)
-    counter1 = Counter(pair1)
-    counter2 = Counter(pair2)
-    inter = list((counter1&counter2).elements())
-    union = list((counter1|counter2).elements())
+    answer = 0
+    inter = 0
+    union = 0
+    str1, str2 = str1.upper() , str2.upper()
+    set_A = Counter([str1[i]+str1[i+1] for i in range(len(str1)-1) if (str1[i]+str1[i+1]).isalpha() ])
+    set_B = Counter([str2[i]+str2[i+1] for i in range(len(str2)-1)if (str2[i]+str2[i+1]).isalpha() ])
     
-    if union:
-        answer = len(inter) / len(union)
-    return int(answer * 65536)
+    inter_elements = list(set_A & set_B)  # 교집합
+    union_elements = list(set_A | set_B)  # 합집합
+    
+    for i in inter_elements:
+        inter += min(set_A[i], set_B[i]) 
+    for i in union_elements:
+        union += max(set_A[i], set_B[i])
+        
+    return int(inter/union * 65536) if inter or union else 65536
