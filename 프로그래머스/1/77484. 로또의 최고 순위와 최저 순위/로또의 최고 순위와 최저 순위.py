7d2a569469ad51a47e5d82864af573a7e3f8cf18
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-10-18 13:00:01 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,9 +1,7 @@
 from collections import Counter
 def solution(lottos, win_nums):
-    point = 0
-    cnt = Counter(lottos)
-    for i in lottos:
-        if i in win_nums:
-            point += 1
-    tar = [6,6,5,4,3,2,1]
-    return tar[cnt[0]+point], tar[point]
+    lottos = Counter(lottos)
+    lottos_num = Counter([i for i in lottos if i])
+    win_nums = Counter(win_nums)
+    rank = [6,6,5,4,3,2,1]
+    return rank[len(lottos_num&win_nums) + lottos[0]] ,rank[len(lottos_num&win_nums)]
