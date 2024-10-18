8d6193abe76cedb443fc8228370558c8f2c022fc
김성규 <61578392+Nacho-Cola@users.noreply.github.com>
2024-10-18 11:01:54 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 92.58 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.py
@@ -1,19 +1,27 @@
-def solution(n, k):
-    answer = 0
-    temp = ''
-    while n>0:
-        temp += str(n%k)
-        n = n // k
-    temp = map(int,list(filter(None, temp[::-1].split('0'))))
-    nums = [n for n in temp if n>1]
-    
-    count = 0
-    for num in nums:
-        for j in range(2,int(num**.5)+1):
-                if not num % j:
-                    count+=1
-                    break
-    
-    return len(nums) - count
+def check_prime (num):
+    if num == 1:
+        return 0
 
+    for p in range(2,int(num**0.5)+1):
+        if not num % p:
+            return 0
+    return 1
 
+def cal(n,k):
+    if k == n:
+        return n
+    answer = -1
+    num = ''
+    while n:
+        num += str(n % k)
+        n //=k
+    return num[::-1]
+
+def solution(n, k): 
+    result = 0
+    prime = [2,3]
+    num = cal(n,k)
+    nums = [int(i) for i in num.split('0') if i]
+    for i in nums:
+        result += check_prime(i)
+    return result
