f507f383a4b8cd47e2f171804d8e75bc03d25b69
Kim Seong-Gyu <61578392+Nacho-Cola@users.noreply.github.com>
2025-01-11 17:34:12 +0900

    [Silver III] Title: 수열, Time: 68 ms, Memory: 40120 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.py
@@ -0,0 +1,11 @@
+N, K = map(int, input().split())
+temperatures = list(map(int, input().split()))
+current_sum = sum(temperatures[:K])
+max_sum = current_sum
+
+for i in range(K, N):
+    current_sum += temperatures[i] - temperatures[i - K]
+    if current_sum > max_sum:
+        max_sum = current_sum
+
+print(max_sum)
