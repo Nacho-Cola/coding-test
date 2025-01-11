ef70257509d56bc6f2e7b98625b58d5b4fd9801a
Kim Seong-Gyu <61578392+Nacho-Cola@users.noreply.github.com>
2025-01-11 14:36:46 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,16 +1,7 @@
 S = list(input())
+S_r = S[::-1]
 
-n = len(S)
-
-if n % 2 :
-    for i in range(n//2):
-        if S[i] != S[-(i+1)]:
-            print(0)
-            exit()
-else :
-    for i in range(n//2):
-        if S[i] != S[-(i+1)]:
-            print(0)
-            exit()
-
-print(1)
+if S == S_r :
+    print(1)
+else:
+    print(0)
